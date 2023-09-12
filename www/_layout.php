a8b39e9567f07c62d9f861594ff83a61a3c741fd
NikitaB1221 <44338356+NikitaB1221@users.noreply.github.com>
2023-09-12 18:35:31 +0300

    Update 2
    Added: 
            + db.php
            + updated access_manager.php
            + navigation update

--- www/_layout.php
+++ www/_layout.php
@@ -13,6 +13,9 @@
 		<div class="nav-wrapper purple accent-3">
 		  <a href="/" class="brand-logo">PV-111</a>
 		  <ul id="nav-mobile" class="right hide-on-med-and-down">
+			<li><a href="about">About</a></li>
+			<li><a href="index">Index</a></li>
+			<li><a href="db">Db</a></li>
 			<li><a href="sass.html">Sass</a></li>
 			<li><a href="badges.html">Components</a></li>
 			<li><a href="collapsible.html">JavaScript</a></li>
@@ -25,4 +28,29 @@
 <!-- Compiled and minified JavaScript -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
+<footer class="page-footer purple accent-3">
+	<div class="container">
+		<div class="row">
+			<div class="col l6 s12">
+				<h5 class="white-text">Footer Content</h5>
+				<p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+				</div>
+				<div class="col l4 offset-l2 s12">
+				<h5 class="white-text">Links</h5>
+				<ul>
+					<li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+					<li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+					<li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+					<li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+				</ul>
+				</div>
+			</div>
+			</div>
+			<div class="footer-copyright">
+			<div class="container">
+			© 2014 Copyright Text
+			<a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+		</div>
+	</div>
+</footer>
 </html>
